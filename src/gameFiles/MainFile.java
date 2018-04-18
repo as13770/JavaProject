8ac6dc318f32d6f53741a95f770082f1359bd1d1
as13770 <as13770@CEIT2552212X004.ad.georgiasouthern.edu>
2018-04-18 16:59:28 -0400

    .01

--- src/gameFiles/MainFile.java
+++ src/gameFiles/MainFile.java
@@ -1,9 +1,19 @@
 package gameFiles;
 
-public class MainFile {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainFile extends Application{
 
 	public static void main(String[] args) {
+		launch();
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.show();
 		
 	}
+
 
 }
